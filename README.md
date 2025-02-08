d761e07a8a3b71e44e1f25f657debbbfc0cf2979
Ziaul Hoda <zhoda315@gmail.com>
2025-02-08 15:08:53 +0530

    db added

--- README.md
+++ README.md
@@ -4,8 +4,8 @@
 - Add Express, jsonwebtoken to it as a dependecy
 - Create index.js
 - Add route skeleton for user login, signup, purchase a course, see course
-- Add middlewares for user and admin auth
-- Add a DB(mongodb), use dotenv to store the DB connection string
 - Define the schema for User, Admin, Course, Purchase
+- Add a DB(mongodb), use dotenv to store the DB connection string
+- Add middlewares for user and admin auth
 - Complete the routes for user login, signup, purchase a course, see course (Extra points - Use express routing to better structure your routes)
 - Create the frontend
---
+++ db.js
@@ -0,0 +1,54 @@
+/*Define the schema for the DB */
+
+const mongoose = require("mongoose");
+// connect to a DB. 
+console.log("DB getting connected")
+mongoose.connect("mongodb://localhost:27017/coursera-app"); 
+// connected to localhost mongoDB 
+
+
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Types.ObjectId;
+
+
+const userSchema = new Schema({
+    email : {type: String, unique: true},
+    password: String,
+    firstName: String,
+    lastName: String
+});
+
+const adminSchema = new Schema({
+    email : {type: String, unique: true},
+    password: String,
+    firstName: String,
+    lastName: String
+});
+
+const courseSchema =new Schema({
+    title:String,
+    description:String,
+    price:Number,
+    imageURL:String,
+    creatorID:ObjectId,
+});
+
+const purchaseSchema =new Schema({
+    userId:ObjectId,
+    courseId:ObjectId
+});
+
+const userModel = mongoose.model("user",userSchema);
+const adminModel = mongoose.model("admin",adminSchema);
+const courseModel = mongoose.model("course",courseSchema);
+const purchaseModel = mongoose.model("purchase",purchaseSchema);
+
+module.export = {
+    userModel,
+    adminModel,
+    courseModel,
+    purchaseModel
+};
+
+
+
